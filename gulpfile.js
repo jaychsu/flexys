092c0451c28669052a076293d393407c1b3b8661
Jaych Su <jaychsu.tw@gmail.com>
2015-10-16 18:08:18 +0800

    Add gulp task to run a webserver

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@
   var minifyCss = require('gulp-minify-css');
   var rename = require('gulp-rename');
   var clean = require('gulp-clean');
+  var autoprefixer = require('gulp-autoprefixer');
+  var browserSync = require('browser-sync').create();
 
   var paths = {
+    root : './',
     src  : './src/',
     dist : './dist/',
     demo : {
@@ -15,19 +18,24 @@
       flexys  : './demo/flexys/',
       sass    : './demo/sass/',
       styles  : './demo/styles/',
-      scripts : './demo/scripts/'
+      scripts : './demo/scripts/',
+      html    : './index.html'
     }
   };
 
   var configs = {
-    server : {
-      host : 'localhost',
-      port : 9000
+    browserSync : {
+      server : paths.root,
+      port   : 9000,
+      ui     : {
+        port : 9001
+      }
     },
-    sass   : {},
-    rename : { suffix: '.min' },
-    clean  : { read: false }
+    sass        : {},
+    rename      : { suffix : '.min' },
+    clean       : { read   : false }
   };
+
 
   /**
    * Task group to release Flexys
@@ -46,6 +54,7 @@
       .pipe(gulp.dest(paths.dist));
   });
 
+
   /**
    * Task group to develop demo pages
    */
@@ -54,14 +63,32 @@
       .pipe(clean());
   });
 
+  gulp.task('demo:html', function () {
+    gulp.src(paths.demo.html)
+      .pipe(browserSync.stream());
+  });
+
+  gulp.task('demo:scripts', function () {
+    gulp.src(paths.demo.scripts + '**/*.js')
+      .pipe(browserSync.stream());
+  });
+
   gulp.task('demo:styles', ['clean:styles'], function () {
     gulp.src(paths.demo.sass + '**/*.scss')
       .pipe(sass(configs.sass).on('error', sass.logError))
-      .pipe(gulp.dest(paths.demo.styles));
+      .pipe(autoprefixer())
+      .pipe(gulp.dest(paths.demo.styles))
+      .pipe(browserSync.stream());
   });
 
-  gulp.task('watch', function () {
+  gulp.task('serve', function () {
+    browserSync.init(configs.browserSync);
+
+    gulp.watch(paths.demo.html, ['demo:html']);
+    gulp.watch(paths.demo.scripts + '**/*.js', ['demo:scripts']);
     gulp.watch(paths.demo.sass + '**/*.scss', ['demo:styles']);
   });
+
+  gulp.task('default', ['serve']);
 
 })();
